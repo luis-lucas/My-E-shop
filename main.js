f4201942115b268c975b29e1ce0a0d65c2563e70
luis-lucas <luislucasluis0@gmail.com>
2026-04-20 13:36:57 +0100

    add hero section with typing animation

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ hamburger.addEventListener ("click", function() {
     navLinks.classList.toggle ("active")
     hamburger.classList.toggle("active")
 })
+ const text= "Welcome to The Noodle Shop — long noodles, short waits. Slurp loud, laugh hard, then nap. No judgment. Just broth."
+ const heroTitle = document.querySelector (".hero-title")
+ let titleCounter = 0
+ let typing = setInterval(() => {
+    heroTitle.textContent += text[titleCounter]
+    titleCounter++
+
+    if(titleCounter === text.length){
+        clearInterval(typing);
+    }
+ }, 100);
+
+     heroTitle.addEventListener("click", function() {
+        clearInterval(typing)
+        heroTitle.textContent = ""
+        titleCounter = 0
+        typing = setInterval(() => {
+            heroTitle.textContent += text[titleCounter]
+            titleCounter++
+            if(titleCounter === text.length){
+        clearInterval(typing);
+    }
+        }, 100);
+    })
